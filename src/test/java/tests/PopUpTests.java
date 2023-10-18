c9952187cfbff67cc0fe4d98ddbd191086e44e30
yuriy.lvatov <lvatov.yuriy@rt.ru>
2023-10-18 21:47:07 +0300

    new refactor

--- src/test/java/tests/PopUpTests.java
+++ src/test/java/tests/PopUpTests.java
@@ -1,7 +1,6 @@
 package tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class PopUpTests extends TestBase {
